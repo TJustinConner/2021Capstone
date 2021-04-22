b4feef0f5e71f21d05bfb8cb27d30a2416830b6e
buddy_white <bjw3j@uvawise.edu>
2021-04-22 11:39:37 -0400

    getting main to work

---
+++ CampusNavigation/app/src/main/java/com/example/campusnavigation/EmailVerifSent.java
@@ -0,0 +1,14 @@
+package com.example.campusnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmailVerifSent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_verif_sent);
+    }
+}
