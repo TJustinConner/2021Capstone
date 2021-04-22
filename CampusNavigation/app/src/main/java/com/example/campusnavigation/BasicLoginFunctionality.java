c99e21a4cd7a0d32dd3eaefe564e44fd6bcc4c70
buddy_white <bjw3j@uvawise.edu>
2021-04-22 09:21:33 -0400

    Gui is made and partially working

---
+++ CampusNavigation/app/src/main/java/com/example/campusnavigation/BasicLoginFunctionality.java
@@ -0,0 +1,142 @@
+package com.example.campusnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.net.ssl.HttpsURLConnection;
+
+//This activity is used for inheritance only, it should not be accessible by the user in the final app
+
+public class BasicLoginFunctionality extends AppCompatActivity {
+
+    private final String getSaltLink = "https://medusa.mcs.uvawise.edu/~jdl8y/getSalt.php";
+    final int MAX_PASSWORD_LENGTH = 32;
+    final int MIN_PASSWORD_LENGTH = 12;
+    final int MAX_EMAIL_LENGTH = 32;
+    final ArrayList<Character> ACCEPTED_SPECIAL_CHARS = new ArrayList<Character>(Arrays.asList('!', '@', '#', '$', '%', '^', '&', '*',
+            '(', ')', '.', '?', '-', '_'));
+
+    //Grabs the salt from the database for the user so we can rehash their password
+    protected String GetSalt(String email){
+        StringBuilder salt = new StringBuilder();
+        URL url = null;
+        HttpsURLConnection conn = null;
+        String data = null;
+        BufferedReader reader = null;
+
+        try {
+            //https://www.tutorialspoint.com/android/android_php_mysql.htm
+            //create a url object and open a connection to the specified link
+            url = new URL(getSaltLink);
+            conn = (HttpsURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+
+            //tries to encode the user's data
+            data = URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8");
+
+            OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+
+            writer.write(data); //send the user's data
+            writer.flush();
+
+            //can't get input stream
+            //read returned message from server
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+
+            while((line = reader.readLine()) != null) {
+                salt.append(line + "\n");
+                //break;
+            } //salt will either be empty or contain an error message if no salt was found
+
+            writer.close();
+        }
+        catch (java.net.MalformedURLException malformedURLException){
+            Log.d("Login","Bad url.");
+        }
+        catch(java.io.UnsupportedEncodingException unsupportedEncodingException){
+            Log.d("Login","Could not encode data.");
+        }
+        catch (java.io.IOException ioException){
+            Log.d("Login","Could not open connection");
+        }
+
+        return salt.toString(); //true if account was created
+    }
+
+    //checks to see if input has the required chars in it, isPassword is true for checking the password, false for the email address
+    //password requires 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character
+    protected boolean ContainsReqCharTypes(String input, boolean isPassword){
+
+        //check the password
+        if(isPassword){
+            boolean containsLowerAlpha = false;
+            boolean containsUpperAlpha = false;
+            boolean containsNumber = false;
+            boolean containsSpecialChar = false;
+            boolean containsInvalidChar = false;
+
+            //check for each needed type of char, check each position
+            for(int i = 0; i < input.length(); i++){
+                if(Character.isLowerCase(input.charAt(i))){
+                    containsLowerAlpha = true;
+                }
+                else if(Character.isUpperCase(input.charAt(i))){
+                    containsUpperAlpha = true;
+                }
+                else if(Character.isDigit(input.charAt(i))){
+                    containsNumber = true;
+                }
+                else if(ACCEPTED_SPECIAL_CHARS.contains(input.charAt(i))){
+                    containsSpecialChar = true;
+                }
+                else{
+                    containsInvalidChar = true;
+                }
+            }
+
+            //if the password meets all char requirements and doesn't contain an invalid char
+            if(containsLowerAlpha && containsNumber && containsSpecialChar && containsUpperAlpha && !containsInvalidChar){
+                return true;
+            }
+            else{
+                Log.d("AcctCreation","Missing Required Char Type(s) or Invalid Char");
+                return false;
+            }
+
+        }
+
+        //check the email
+        else{
+            if(input.contains("@uvawise.edu") || input.contains("@mcs.uvawise.edu") || input.contains("@virginia.edu")){
+                return true;
+            }
+            else{
+                Log.d("AcctCreation","Missing Correct Email Domain");
+                return false;
+            }
+        }
+    }
+
+    protected boolean InputLengthIsGood(String email, String password){
+        if(password.length() >= MIN_PASSWORD_LENGTH && password.length() <= MAX_PASSWORD_LENGTH && email.length() <= MAX_EMAIL_LENGTH){
+            return true;
+        }
+
+        else{
+            Log.d("AcctCreation","Invalid Input Length");
+            return false;
+        }
+    }
+}
---
+++ CampusNavigation/app/src/main/java/com/example/campusnavigation/BulletinBoardGUI.java
@@ -0,0 +1,156 @@
+package com.example.campusnavigation;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class BulletinBoardGUI extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private String location;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bulletin_board_g_u_i);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        Log.d("DEBUG", "Java sucks dick.");
+        //This is the setup of the Locations spinner dropdown menu
+        Spinner spinner = (Spinner) findViewById(R.id.locationSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Locations, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+    protected static String eventSearch(String location) throws IOException {//query the database with the location that was given
+        URL url = null;
+        HttpsURLConnection urlConnection = null;;
+        BufferedReader reader = null;
+
+        String input = location;
+        String output = "";
+
+        //prepare the data for the query by encoding
+        input = URLEncoder.encode("location", "UTF-8")
+                + "=" + URLEncoder.encode(input, "UTF-8");
+
+        //set the url to the php file in the student account, make the connection
+        url = new URL("https://medusa.mcs.uvawise.edu/~jwe3nv/search.php");
+        urlConnection = (HttpsURLConnection) url.openConnection();
+        Log.d("SUCCESS", "Made connection. totalInput is: " + input);
+
+        //make the actual connection, pass the input data into the writer, and run the php
+        urlConnection.setDoOutput(true);
+        OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+        wr.write(input);
+        wr.flush();
+        Log.d("BREAKPOINT", "PASSED OUTPUT");
+
+        //this is taking the echo/returned stuff from the database and saving it in text
+        reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+        output = sb.toString();
+        Log.d("SUCCESS", "The SQL QUERY RETURNED:" + output);
+
+        output.trim();
+        String outputS[] = output.split("\n");
+        String temp[];
+        Integer int1, int2;
+        output = "";
+        boolean flag = false;
+
+        for (int i = 0; i < outputS.length; i += 1){
+            if (outputS[i].contains("id")){//finds the id line
+                i += 1;
+                if(outputS[i].indexOf('(') != -1){//finds a " in the string
+                    Log.d("DEBUG", "found the first quote");
+                    int1 = outputS[i].indexOf('(');//saves the first pos
+                    int2 = outputS[i].indexOf(')');//saves the second pos
+                    output += "ID: " + outputS[i].substring(int1+1, int2) + "\n";
+                }
+            }
+            else if (outputS[i].contains("name")){//found the name field
+                Log.d("DEBUG", "Found the name string.");
+                i +=1;
+                int1 = outputS[i].indexOf(')');
+                output += "NAME: " + outputS[i].substring(int1 +3, outputS[i].length() - 1) + "\n";
+            }
+            else if (outputS[i].contains("Time")){
+                i +=1;
+                int1 = outputS[i].indexOf(')');
+                output += "TIME: " + outputS[i].substring(int1 +3, outputS[i].length() - 1) + "\n";
+            }
+            else if (outputS[i].contains("eTime")){
+                i +=1;
+                int1 = outputS[i].indexOf(')');
+                output += "ETIME: " + outputS[i].substring(int1 +3, outputS[i].length() - 1) + "\n";
+            }
+            else if (outputS[i].contains("Date")){
+                i +=1;
+                int1 = outputS[i].indexOf(')');
+                output += "DATE: " + outputS[i].substring(int1 +3, outputS[i].length() - 1) + "\n";
+            }
+            else if (outputS[i].contains("recur")){
+                i +=1;
+                int1 = outputS[i].indexOf(')');
+                output += "RECUR: " + outputS[i].substring(int1 +3, outputS[i].length() - 1) + "\n";;
+            }
+            else if (outputS[i].contains("location")){
+                i +=1;
+                int1 = outputS[i].indexOf(')');
+                output += "LOCATION: " + outputS[i].substring(int1 +3, outputS[i].length() - 1) + "\n";
+            }
+            else if (outputS[i].contains("description")){
+                i +=1;
+                int1 = outputS[i].indexOf(')');
+                output += "DESCRIPTION: " + outputS[i].substring(int1 +3, outputS[i].length() - 1) + "\n";
+            }
+            else if (outputS[i].contains("image")){
+                i +=1;
+                int1 = outputS[i].indexOf(')');
+                output += "BLOB: " + outputS[i].substring(int1 +3, outputS[i].length() - 1) + "\n";
+            }
+        }
+        Log.d("SUCCESS", "The trimmed output is: " + output);
+        return output;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        try {
+            Log.d("DEBUG", "Results of test are: " + eventSearch(parent.getItemAtPosition(position).toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
