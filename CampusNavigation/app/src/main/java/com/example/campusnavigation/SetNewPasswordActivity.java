0e0f81d1e362c0da4061ef7614b469874771fc41
Jonathan Large <jonathanlarge1@hotmail.com>
2021-04-30 13:43:34 -0400

    Fixed clipping on SetNewPassword follow guidelines error

--- CampusNavigation/app/src/main/java/com/example/campusnavigation/SetNewPasswordActivity.java
+++ CampusNavigation/app/src/main/java/com/example/campusnavigation/SetNewPasswordActivity.java
@@ -1,10 +1,7 @@
 package com.example.campusnavigation;
 
 import android.content.Intent;
-import android.location.SettingInjectorService;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
